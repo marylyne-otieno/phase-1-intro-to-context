1274403697f82fa393505a02061998ea1b0175af
marylyne <otienomarylyne76@gmail.com>
2025-03-21 22:39:08 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Your code here
+function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
+  return {
+      firstName,
+      familyName,
+      title,
+      payPerHour,
+      timeInEvents: [],
+      timeOutEvents: []
+  };
+}
+function createEmployeeRecords(employeeData) {
+  return employeeData.map(createEmployeeRecord);
+}
+function createTimeInEvent(employee, dateTime) {
+  let [date, hour] = dateTime.split(" ");
+  employee.timeInEvents.push({ type: "TimeIn", date, hour: parseInt(hour) });
+  return employee;
+}
+function createTimeOutEvent(employee, dateTime) {
+  let [date, hour] = dateTime.split(" ");
+  employee.timeOutEvents.push({ type: "TimeOut", date, hour: parseInt(hour) });
+  return employee;
+}
+function hoursWorkedOnDate(employee, date) {
+  let timeIn = employee.timeInEvents.find(e => e.date === date);
+  let timeOut = employee.timeOutEvents.find(e => e.date === date);
+  return (timeOut.hour - timeIn.hour) / 100;
+}
+function wagesEarnedOnDate(employee, date) {
+  return hoursWorkedOnDate(employee, date) * employee.payPerHour;
+}
+function calculatePayroll(employees) {
+  return employees.reduce((total, employee) => total + allWagesFor(employee), 0);
+}
+
+function allWagesFor(employee) {
+  return employee.timeInEvents.reduce((total, timeIn) => {
+      let date = timeIn.date;
+      return total + wagesEarnedOnDate(employee, date);
+  }, 0);
+}
